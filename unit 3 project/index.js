22e54b16a059c629a841ed6a30ce277f1b5cf718
myronpereira3 <myronpereira3@gmail.com>
2022-05-06 16:29:13 +0530

    edited

--- unit 3 project/index.js
+++ unit 3 project/index.js
@@ -1,2 +1,4 @@
-import { footer } from "./component/footer.js";
+import { footer } from "../components/footer.js";
+import { navbar } from "../components/navbar.js";
 document.getElementById("footermainpagewrapperfun").innerHTML=footer();
+document.getElementById("navhomepage").innerHTML=navbar();
