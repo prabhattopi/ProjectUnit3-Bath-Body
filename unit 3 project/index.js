9d8f2cab8dad639ddb26e053727d57e40e7e6919
prabhattopi <topi9864@gmail.com>
2022-05-06 21:06:59 +0530

    ifhsdfjisd

--- unit 3 project/index.js
+++ unit 3 project/index.js
@@ -2,3 +2,20 @@ import { footer } from "../components/footer.js";
 import { navbar } from "../components/navbar.js";
 document.getElementById("footermainpagewrapperfun").innerHTML=footer();
 document.getElementById("navhomepage").innerHTML=navbar();
+
+
+
+let na=document.querySelectorAll(".conta")
+let scrollPrevious=window.pageYOffset;
+window.onscroll=function(){
+    let scrollCurrent=window.pageYOffset;
+    if(scrollPrevious>scrollCurrent){
+        na.style.top="10px";
+
+    }
+    else{
+        na.style.top="-90px"
+
+    }
+    scrollPrevious=scrollCurrent
+}
