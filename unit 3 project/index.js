14181b9b4540a9a54050076bb0a0283c02680e4f
prabhattopi <topi9864@gmail.com>
2022-05-06 21:16:00 +0530

    oidshfdsjiosdf

--- unit 3 project/index.js
+++ unit 3 project/index.js
@@ -2,3 +2,17 @@ import { footer } from "../components/footer.js";
 import { navbar } from "../components/navbar.js";
 document.getElementById("footermainpagewrapperfun").innerHTML=footer();
 document.getElementById("navhomepage").innerHTML=navbar();
+
+let na=document.querySelector("nav")
+let scrollPrevious=window.pageYOffset;
+window.onscroll=function(){
+    let scrollCurrent=window.pageYOffset;
+    if(scrollPrevious >scrollCurrent){
+        na.style.top="50px"
+
+    }
+    else{
+        na.style.top="-90px"
+    }
+    scrollPrevious=scrollCurrent
+}
