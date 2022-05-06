e3d3f1eb984edd602e4e585842dd534dfd533df5
dambale123 <shubhamdambale123@gmail.com>
2022-05-06 19:17:53 +0530

    new added

---
+++ Products/Gifts/mothers_day_gift.js
@@ -0,0 +1,253 @@
+var mothers_day_gifts = [
+
+
+    {
+        no:1,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf97a6473/crop/026414639_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "",
+        name: "Butterfly",
+        about: "Fine Fragrance Mist",
+        price: " 17.50",
+        discount: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+        caterory: "new"
+    },
+    
+    {
+        no:2,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw0f97819c/crop/026473987_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "New Fragrance",
+        name: "Butterfly",
+        about: "3-Wick Candle",
+        price: " 12.95",
+        discount: "",
+        caterory: "old"
+    },
+    
+    {
+        no:3,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw744d9e74/crop/026414637_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "",
+        name: "Butterfly",
+        about: "Daily Nourishing Body Lotion",
+        price: " 15.50",
+        discount: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Frea",
+        caterory: "new"
+    },
+    
+    {
+        no:4,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf22e6758/hires/026414645_alt_1.jpg?sh=413&yocs=o_s_",
+        status: "",
+        name: "Butterfly",
+        about: "Creamy Body Scrub",
+        price: " 17.50",
+        discount: "Mix &Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+        caterory: "old"
+    },
+    
+    {
+        no:5,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw3db81df1/crop/026413951_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "",
+        name: "Butterfly",
+        about: "Mini Perfume Spray",
+        price: " 13.50",
+        discount: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+        caterory: "new"
+    },
+    
+    
+    {
+        no:6,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw8b383976/crop/026413955_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "",
+        name: "Butterfly",
+        about: "Bubble Bath",
+        price: " 17.50",
+        discount: "Mix &Match: Buy 3, Get 3 Free or Buy 2, Get 1 Free",
+        caterory: "old"
+    },
+    
+    {
+        no:7,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw54c12225/crop/026439201_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "New Fragrance",
+        name: "Butterfly",
+        about: "Gentle Foaming Hand Soap",
+        price: " 7.50",
+        discount: "Mix & Match: 6/$27 or 4/$20",
+        caterory: "new"
+    },
+    
+    {
+        no:8,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw850c06fd/crop/025144309_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "New!",
+        name: "Gingham",
+        about: "Gift Box Set",
+        price: " 34.95",
+        discount: "",
+        caterory: "old"
+    },
+    
+    {
+        no:9,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw816a87d5/crop/025144299_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "New!",
+        name: "Sunshine Mimosa",
+        about: "Gift Box Set",
+        price: " 34.95",
+        discount: "",
+        caterory: "new"
+    },
+    
+    {
+        no:10,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc7ed5e1a/crop/026439710_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "New Fragrance",
+        name: "Tulip Gardens",
+        about: "Gentle Foaming Hand Soap",
+        price: " 7.50",
+        discount: "Mix & Match: 6/827 or 4/$20",
+        caterory: "old"
+    },
+    
+    {
+        no:11,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc7d5bc20/crop/026460822_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "",
+        name: "Sweet Mint& Rain",
+        about: "Gentle Foaming Hand Soap",
+        price: " 8.50",
+        discount: "Mox&Match: 5/s27 or 4/s20",
+        caterory: "new"
+    },
+    
+    {
+        no:12,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw0e4ae707/crop/026438944_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "",
+        name: "Pineapple Prosecco",
+        about: "Gentle Foaming Hand Soap",
+        price: " 7.50",
+        discount: "Mix &Match: 6/$27 or 4/$20",
+        caterory: "old"
+    },
+    
+    {
+        no:13,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw76528099/crop/026468308_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "New Fragrance",
+        name: "Bronzed Sunset",
+        about: "Gentle Foaming Hand Soap",
+        price: " 7.50",
+        discount: "Mix & Match: 6/27 or 4/$20",
+        caterory: "new"
+    },
+    
+    {
+        no:14,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwae11afdf/crop/026438528_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "",
+        name: "Japanese Cherry Blossom",
+        about: "Fine Fragrance Mist",
+        price: " 16.50",
+        discount: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Frea",
+        caterory: "old"
+    },
+
+    {
+        no:15,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw73102edd/crop/026400331_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        status: "",
+        name: "Champagne Toast",
+        about: "Fine Fragrance Mist",
+        price: " 17.50",
+        discount: "Mix & Match: Buy 3, Get 3 Free or Buy 2, Get 1 Frea",
+        caterory: "new"
+    },
+
+    {
+        no:16,
+        image: "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw54dd1987/hires/026458610_alt_1.jpg?sh=413&yocs=o_s_",
+        status: "",
+        name: "Pink Lemonade",
+        about: "3-Wick Candle",
+        price: " 12.95",
+        discount: "",
+        caterory: "old"
+    },
+    
+  
+    ];
+    
+    
+    var cartData = JSON.parse(localStorage.getItem("cart")) || [];
+    
+    
+    
+    DisplayData(mothers_day_gifts);
+    
+    function DisplayData(mothers_day_gifts){
+        document.querySelector("#container").innerHTML= "";
+      
+      
+      
+      
+      
+      
+      
+        mothers_day_gifts.map(function(elem){
+      
+          var box = document.createElement("div");
+         
+      
+          var img = document.createElement("img");
+          img.setAttribute("class", "img_div")
+          img.src = elem.image;
+      
+          var status = document.createElement("p");
+          status.setAttribute("class", "status_div")
+          status.innerText = elem.status
+    
+          var names = document.createElement("h3");
+          names.setAttribute("class", "names_div")
+          names.textContent = elem.name;
+    
+          var about = document.createElement("p");
+          about.setAttribute("class", "about_div")
+          about.innerText = elem.about;
+      
+          var prices = document.createElement("p");
+          prices.setAttribute("class", "prices_div")
+          prices.innerText =" "+elem.price;
+    
+          var discount = document.createElement("p")
+          discount.setAttribute("class", "discount_div")
+          discount.innerText = elem.discount;
+      
+      
+           var btn = document.createElement("button");
+           btn.setAttribute("class", "buttons_div");
+           btn.innerText = "Add To Cart";
+           btn.addEventListener("click", function() {
+              addToCart(elem);
+           })
+      
+           
+          
+      
+          box.append(img,status,names,about,prices,discount,btn);
+      
+          document.querySelector("#container").append(box);
+      })
+    
+    
+    
+      function addToCart(elem){
+        console.log(elem);
+        cartData.push(elem);
+        localStorage.setItem("cart", JSON.stringify(cartData));
+        alert("item added to cart")
+    }
+    }
